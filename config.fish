3b4748e8febd062b20a380a69a8ee445f59bb5b3
Nat Dempkowski <natdempk@gmail.com>
2018-03-12 22:39:23 -0400

    Add fish config

---
+++ config.fish
@@ -0,0 +1,6 @@
+#
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/natdempk/Downloads/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/natdempk/Downloads/google-cloud-sdk/path.fish.inc'; else; . '/Users/natdempk/Downloads/google-cloud-sdk/path.fish.inc'; end; end
+
+# rust cargo
+set PATH $HOME/.cargo/bin $PATH
